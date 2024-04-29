86d1f4ca8688ba1d4ff8d610e909cee8db83e6d3
wfrotj <trebrodrigo@gmail.com>
2024-04-29 21:11:39 +0800

    reference varaible

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { SampleComponent } from './sample/sample.component';
 
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { SearchCustomerComponent } from './search-customer/search-customer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SampleComponent, CustomerListComponent],
+  imports: [
+    RouterOutlet,
+    SampleComponent,
+    CustomerListComponent,
+    SearchCustomerComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
